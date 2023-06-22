205011fbbdb2340ea8e828e6f36d6c5064d1470e
Roman <email>
2023-06-22 21:58:11 +0600

    rm gas price

--- src/contract/contract.ts
+++ src/contract/contract.ts
@@ -5,14 +5,12 @@ import geoIp from "fast-geoip"
 import ABI from "./ABI"
 
 const WEB3_GAS_LIMIT = Number(process.env.WEB3_GAS_LIMIT) || 3000000
-const WEB3_GAS_PRICE = process.env.WEB3_GAS_PRICE || "4000000000"
 
 const provider = new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER!)
 const web3 = new Web3(provider)
 
 const contract = new web3.eth.Contract(ABI, process.env.CONTRACT_ADDRESS, {
   gas: WEB3_GAS_LIMIT,
-  gasPrice: WEB3_GAS_PRICE,
 })
 
 interface IAllNodeResponseItem {
